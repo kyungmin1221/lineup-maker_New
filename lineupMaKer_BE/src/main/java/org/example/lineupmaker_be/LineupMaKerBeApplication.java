e1734d009411b78144047eb26ca72b1882002e04
kpark26 <kpark26@kr.kpmg.com>
2026-07-29 10:08:38 +0900

    Initial commit

---
+++ lineupMaKer_BE/src/main/java/org/example/lineupmaker_be/LineupMaKerBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.lineupmaker_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineupMaKerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineupMaKerBeApplication.class, args);
+    }
+
+}
---
+++ lineupMaKer_BE/src/test/java/org/example/lineupmaker_be/LineupMaKerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lineupmaker_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineupMaKerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
